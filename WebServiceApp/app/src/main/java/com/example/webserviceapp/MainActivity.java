7d77494cf430cd6d99507c2d381efd72b9e2ecdc
reeccardo <94625621+reeccardo@users.noreply.github.com>
2022-02-08 12:50:50 +0100

    manifest e commento consegna

--- WebServiceApp/app/src/main/java/com/example/webserviceapp/MainActivity.java
+++ WebServiceApp/app/src/main/java/com/example/webserviceapp/MainActivity.java
@@ -6,6 +6,12 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    /*
+    primo pulsante: sync con la progress bar
+    secondo pulsante: dowloand img da internet e show sotto il pulsante
+    terzo pulsante: utilizzo di retrofit con i metodi rest
+     */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
